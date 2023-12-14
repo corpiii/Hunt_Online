90acc61dac80327f5ceb811e7e852f3cd9a55bd3
corpi <lj845854@gmail.com>
2023-12-14 13:04:44 +0900

    초기 프로젝트 세팅

---
+++ bin/hunt_online.dart
@@ -0,0 +1,5 @@
+import 'package:hunt_online/hunt_online.dart' as hunt_online;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hunt_online.calculate()}!');
+}
---
+++ test/hunt_online_test.dart
@@ -0,0 +1,8 @@
+import 'package:hunt_online/hunt_online.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
